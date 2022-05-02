6fcd71b1d3526899951e126f11533c2a826e8558
Bradley Matola <brmatola@gmail.com>
2022-05-02 17:25:28 -0400

    config should fail if no backend url provided

--- pages/api/config.ts
+++ pages/api/config.ts
@@ -1,7 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    const backendUrl = process.env.BACKEND_URL
+    if (!backendUrl) throw new Error('No Backend Url configured')
+
     res.status(200).json({
-        apiUrl: process.env.BACKEND_URL
+        apiUrl: backendUrl
     })
 }
